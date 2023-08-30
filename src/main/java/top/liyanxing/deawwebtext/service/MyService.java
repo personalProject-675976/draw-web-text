76e7a112f3c7ad40664801a189ac888cb72a2b4a
李艳兴 <liyanxingelon@gmail.com>
2023-08-30 11:57:29 +0800

    feat: 完成

--- src/main/java/top/liyanxing/deawwebtext/service/MyService.java
+++ src/main/java/top/liyanxing/deawwebtext/service/MyService.java
@@ -1,20 +1,32 @@
 package top.liyanxing.deawwebtext.service;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+import top.liyanxing.common.CommonResult;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class MyService
 {
     private static final List<String> TAG_NAME_LIST = CollUtil.newArrayList("h1", "h2", "h3", "h4", "h5", "h6", "p", "div");
 
     /**
      * 提取网页上的文字
-     * <hr/>
      * @param url 网页url
      * @return 文字
      */
     public CommonResult<String> getText(String url)
     {
+        Assert.notBlank(url, "url不能为空");
         List<Element> eleOfHPD = this.getEleOfHPD(url);
         String htmlText = eleOfHPD.stream().map(Element::toString).collect(Collectors.joining());
 
@@ -23,7 +35,6 @@ public class MyService
 
     /**
      * 获得所有的最外层 hx p div 标签
-     * <hr/>
      * @param url url
      * @return elList
      */
